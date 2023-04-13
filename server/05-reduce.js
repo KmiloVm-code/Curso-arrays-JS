4a4ab4c455765e44c85966ff24306c40c20599ae
Andres Camilo Velez Mejia <kmilo7v@gmail.com>
2023-04-13 16:01:48 -0500

    Se agrega ejemplo funcion reduce

--- server/05-reduce.js
+++ server/05-reduce.js
@@ -9,3 +9,7 @@ console.log(sum);
 
 const rta = totals.reduce((sum, element) => sum + element, 0)
 console.log('rta', rta);
+
+const arr = [1, 2, 3, 4, 5];
+console.log(arr.reduce((acc, curr) => [curr, ...acc], []));
+//Output: [ 5, 4, 3, 2, 1 ]
